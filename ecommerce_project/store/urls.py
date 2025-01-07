4b3f8a534c47d54895dd04ca962661f663f4294a
dineshsibi <dineshsibi11@gmail.com>
2025-01-07 14:01:18 +0530

    Delete ecommerce_project directory

--- ecommerce_project/store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('order/<int:product_id>/', views.place_order, name='order'),
-    path('order-success/', views.order_success, name='order_success'),
-]
-
